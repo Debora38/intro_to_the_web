66f683dbf63c5eb9fc71414840d56024ebfa9ec9
Debora <debora.piu92@gmail.com>
2019-01-22 16:25:32 +0000

    form separated from cat pic for name input

--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
 
-get '/cat' do
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/cat_form' do
+  erb(:cat_form)
+end
+
+post '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
